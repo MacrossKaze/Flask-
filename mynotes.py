32f2f9daeaa2c777f8522bae7438e136f16c6886
jxHong <vasnstar@gmail.com>
2020-02-08 00:19:10 +0800

    flask case finished

---
+++ mynotes.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, session
+from flask_session.__init__ import Session
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+#mynotes = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if session.get("mynotes") is None:
+        session["mynotes"] = []
+    if request.method == "POST":
+        note = request.form.get("note")
+        session["mynotes"].append(note)
+
+    return render_template("index.html", mynotes=session["mynotes"])
